d6acd3be30f122065319dd60c02c99858db71940
Kade <61953866+kdc24@users.noreply.github.com>
2022-05-03 13:54:56 +1200

    Update assesment_thing_v2.ino

--- assesment_thing_v2/assesment_thing_v2.ino
+++ assesment_thing_v2/assesment_thing_v2.ino
@@ -1,26 +1,42 @@
 
 #include <Wire.h>
-#include "SparkFun_Qwiic_OpenLog_Arduino_Library.h"
-#include <SparkFun_LPS25HB_Arduino_Library.h> // Click here to get the library: http://librarymanager/All#SparkFun_LPS25HB
-LPS25HB sensorBoard; // Create an object of the LPS25HB class
+#include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
+#include <SparkFun_LPS25HB_Arduino_Library.h>
+#include <Servo.h>
+
+//----------------------------
+//
+//----------------------------
+LPS25HB sensorBoard;
 OpenLog dataLog;
+Servo myservo;
 
-const byte hPa = 8;
+//----------------------------
+//
+//----------------------------
+int spos = 0;
+int epos = 90;
+const byte SERVO_PIN = 9;
 
-//test
+//----------------------------
+//
+//----------------------------
 void setup()
 {
   Serial.begin(9600);
 
-
+  myservo.attach(SERVO_PIN);
   Wire.begin();
   dataLog.begin();
-  sensorBoard.begin(Wire, LPS25HB_I2C_ADDR_DEF); // Begin with I2C settings of your choice (see Example2_I2C_Configuration)
+  sensorBoard.begin(Wire, LPS25HB_I2C_ADDR_DEF);
   dataLog.append("Kade.txt");
   dataLog.println("Pressure, Temp");
-
+  myservo.write(spos);
 }
 
+//----------------------------
+//
+//----------------------------
 void loop()
 {
 
@@ -29,15 +45,21 @@ void loop()
     if (sensorBoard.getStatus() == 0x00)
     {
       sensorBoard.begin();
-    } // If it is connected but not responding (for example after a hot-swap) then it may need to be re-initialized
-    Serial.print(", Pressure (hPa): ");
-    Serial.print(sensorBoard.getPressure_hPa()); // Get the pressure reading in hPa as determined by dividing the number of ADC counts by 4096 (according to the datasheet)
+    }
+    //----------------------------
+    //
+    //----------------------------
+    Serial.print("Pressure (hPa): ");
+    Serial.print(sensorBoard.getPressure_hPa());
     Serial.print(", Temperature (degC): ");
-    Serial.println(sensorBoard.getTemperature_degC()); // Get the temperature in degrees C by dividing the ADC count by 480
-    dataLog.print(", Pressure (hPa): ");
-    dataLog.print(sensorBoard.getPressure_hPa()); // Get the pressure reading in hPa as determined by dividing the number of ADC counts by 4096 (according to the datasheet)
-    dataLog.print(", Temperature (degC): ");
-    dataLog.println(sensorBoard.getTemperature_degC()); // Get the temperature in degrees C by dividing the ADC count by 480
+    Serial.println(sensorBoard.getTemperature_degC());
+
+    //----------------------------
+    //
+    //----------------------------
+    dataLog.print(sensorBoard.getPressure_hPa());
+    dataLog.print(",");
+    dataLog.println(sensorBoard.getTemperature_degC());
     dataLog.syncFile();
 
   }
@@ -47,4 +69,14 @@ void loop()
     sensorBoard.begin();
   }
   delay(100);
+
+  //----------------------------
+  //
+  //----------------------------
+//   if () // when should i deploy the chute? // millis()
+  {
+    myservo.write(epos);
+  }
+}
+
 }
