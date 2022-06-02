e5be38c17c319e59ad8195f9669012b21382e42d
Kade <61953866+kdc24@users.noreply.github.com>
2022-06-02 22:39:04 +1200

    Update assesment_thing_v2.ino

--- assesment_thing_v2/assesment_thing_v2.ino
+++ assesment_thing_v2/assesment_thing_v2.ino
@@ -4,23 +4,18 @@
 #include <SparkFun_LPS25HB_Arduino_Library.h>
 #include <Servo.h>
 
-//----------------------------
-//
-//----------------------------
+//this sets up the sensor board, datalog and the servo
 LPS25HB sensorBoard;
 OpenLog dataLog;
 Servo myservo;
 
-//----------------------------
-//
-//----------------------------
+//these are my data types i have my "end position" and "start position" as intergers and setting up my servo to pin 9 as a const
 int spos = 0;
 int epos = 90;
+const byte pressue = 1000;
 const byte SERVO_PIN = 9;
 
-//----------------------------
-//
-//----------------------------
+//this is setting up the serial monitor. starting the datalog, sensorboard.
 void setup()
 {
   Serial.begin(9600);
@@ -34,9 +29,7 @@ void setup()
   myservo.write(spos);
 }
 
-//----------------------------
-//
-//----------------------------
+//these are the if statments, it makes sure the sensorboard is connected and it grabs the status of it
 void loop()
 {
 
@@ -46,17 +39,13 @@ void loop()
     {
       sensorBoard.begin();
     }
-    //----------------------------
-    //
-    //----------------------------
+    //this prints to my serial monitor the hpa and the temp
     Serial.print("Pressure (hPa): ");
     Serial.print(sensorBoard.getPressure_hPa());
     Serial.print(", Temperature (degC): ");
     Serial.println(sensorBoard.getTemperature_degC());
 
-    //----------------------------
-    //
-    //----------------------------
+    //this gets the all of the data onto the sync file
     dataLog.print(sensorBoard.getPressure_hPa());
     dataLog.print(",");
     dataLog.println(sensorBoard.getTemperature_degC());
@@ -70,11 +59,14 @@ void loop()
   }
   delay(100);
 
-  //----------------------------
-  //
-  //----------------------------
-  //   if () // when should i deploy the chute? // millis()
-  {
+  //if statment to get my shute to open
+  //if (getPressure_hPa < 1031) 
+  //{deployShute ();
+  }
+ // else { 
+   // SERVO_PIN.write(ShuteClose);
+ // }
+ {
     myservo.write(epos);
   }
 }
